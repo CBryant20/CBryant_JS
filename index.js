e6db28b0e6850794fd77b7c7f024abaa19a47769
Charles Bryant <cwbryant25@yahoo.com>
2024-01-17 12:12:52 -0800

    JS_assignment

---
+++ index.js
@@ -0,0 +1,10 @@
+// Create a string giving a message to the user
+const message =
+  "You have received this message because you have been chosen to open an important vault. Here is the secret combination:";
+
+// Declare three variables using arithmetic operators to equal each combo number
+const comboNum1 = 5 + 5;
+const comboNum2 = 66 - 13 * 2;
+const comboNum3 = 12 + 81 / 3;
+
+alert(`${comboNum1} - ${comboNum2} - ${comboNum3}`);
